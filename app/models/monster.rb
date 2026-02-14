b37e1b7383ef3ca92b4e230bac7281ce5cfb7b67
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-14 15:33:46 +0900

    set up database relationship and test data

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,8 @@
 class Monster < ApplicationRecord
   belongs_to :student
+  has_many :tasks, through: :monster_tasks
+
+  validates :student_id, presence: true
+  validates :name, presence: true
+
 end
