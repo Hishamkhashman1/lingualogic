03c6328a8e92cc4fedf5e979c089c28f1cbf8b1d
Hisham <hisham.khashman@gmail.com>
2026-02-19 21:21:41 +0900

    1st version monster controller

--- app/controllers/monster_controller.rb
+++ app/controllers/monster_controller.rb
@@ -1,2 +1,16 @@
 class MonsterController < ApplicationController
+
+  def new
+    @monster = Monster.new
+  end
+
+  def create
+    @monster = Monster.new(:name, :species_type)
+    @monster.student = current_student
+  end
+
+  def show
+    @monster = Monster.find(params[:id])
+  end
+
 end
