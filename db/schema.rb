b2fd501950a983096a6f283a7c46717c2a353a09
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-21 14:39:13 +0900

    add images to monsters model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_21_025118) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_21_051918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_21_025118) do
     t.string "species_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["student_id"], name: "index_monsters_on_student_id"
   end
 
