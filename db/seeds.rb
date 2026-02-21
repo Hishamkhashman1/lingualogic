b181f7c47d524a3ba604d92be6ea9c6dd87ae68a
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-21 14:07:16 +0900

    created basic test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+
+Monster.destroy_all
+Task.destroy_all
+
+puts "Creating test seeds..."
+
+# test_user = Student.create!(email: "testing@testing.test", encrypted_password: "123456", reset_password_token: "123456");
+
+test_monster = Monster.create!(name: "Testasaur", student_id: 1, happiness: 1, energy: 100, species_type: "bulbasaur");
+
+test_task = Task.create!(goal: "Set speed to 800", difficulty: 0)
+
+puts "Seeding finished."
