c94eccf506ff006f825d7878d5d938d759b12622
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-03-10 22:41:13 +0900

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,11 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# puts "Cleaning database..."
-Item.destroy_all
+puts "Cleaning database..."
+Notification.destroy_all
+StudentItem.destroy_all
 MonsterTask.destroy_all
+Item.destroy_all
 Task.destroy_all
 Monster.destroy_all
 Student.destroy_all
@@ -19,14 +21,14 @@ puts "Creating test seeds..."
 
 test_user = Student.create!(email: "testing@testing.test", password: "123456");
 
-# test_monster = Monster.create!(name: "Testasaur", student_id: 1, happiness: 1, energy: 100, species_type: "bulbasaur", image: "https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png");
-
-# test_task = Task.create!(goal: "Set speed to 800", difficulty: 0)
+test_monster = Monster.create!(name: "Testasaur", student_id: 1, health: 50, energy: 50, species_type: "bulbasaur", image: "https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png");
 
 task_level1 = Task.create!(goal: "First task", difficulty: 1, reward_exp: 100, reward_item1: "apple", reward_item2: "funny_hat", name: "level1")
-task_level5 = Task.create!(goal: "Feed monster", difficulty: 2, reward_exp: 150, reward_health: 20, reward_energy: 10, reward_item1: "apple", name: "level5")
+task_level5 = Task.create!(goal: "Feed monster", difficulty: 5, reward_exp: 150, reward_health: 20, reward_energy: 10, reward_item1: "apple", name: "level5")
+task_level6 = Task.create!(goal: "Get energy", difficulty: 6, reward_exp: 150, reward_health: 0, reward_energy: 25, reward_item1: "apple", name: "level6")
+
 
 item_apple = Item.create!(price: 25, description: "+ energy", accessory: false, name: "apple", image: "apple-s.png")
-item_hat = Item.create!(price: 100, description: "hat", accessory: true, name: "funny_hat", image: "top_hat.png")
+item_hat = Item.create!(price: 100, description: "hat", accessory: true, name: "funny hat", image: "top-hat.png")
 
 puts "Seeding finished."
