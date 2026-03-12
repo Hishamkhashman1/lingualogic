6c7eb57457ea4d20c57ec536c54a503868e4ea92
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-03-12 21:52:59 +0900

    added level 2 3 4 seeds to level 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,9 @@ test_user = Student.create!(email: "testing@testing.test", password: "123456");
 test_monster = Monster.create!(name: "Testasaur", student_id: test_user.id, health: 50, energy: 50, species_type: "bulbasaur", image: "https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png");
 
 task_level1 = Task.create!(goal: "First task", difficulty: 1, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level1")
-task_level2 = Task.create!(goal: "Learn to jump", difficulty: 2, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level1")
-task_level3 = Task.create!(goal: "IF command", difficulty: 3, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level1")
-task_level4 = Task.create!(goal: "AND command", difficulty: 4, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level1")
+task_level2 = Task.create!(goal: "Learn to jump", difficulty: 2, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level5")
+task_level3 = Task.create!(goal: "IF command", difficulty: 3, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level5")
+task_level4 = Task.create!(goal: "AND command", difficulty: 4, reward_exp: 100, reward_item1: "apple", reward_item2: "funny hat", name: "level5")
 task_level5 = Task.create!(goal: "Feed monster", difficulty: 5, reward_exp: 150, reward_health: 20, reward_energy: 10, reward_item1: "apple", name: "level5")
 task_level6 = Task.create!(goal: "Get energy - OR command", difficulty: 6, reward_exp: 150, reward_health: 0, reward_energy: 25, reward_item1: "apple", name: "level6")
 
